1727992649e99615246cd2976e3e516810a3407f
Chetna Negi <chetna.negi@synapse.com>
2018-12-18 17:37:54 +0530

    Adding POJOs.

---
+++ src/main/java/cnegi/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package cnegi.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/cnegi/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package cnegi.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
